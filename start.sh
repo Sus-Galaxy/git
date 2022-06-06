2f196ce43bd23b74af33eedb65ce127ee5e622ad
InikoMatthew <89306317+InikoMatthew@users.noreply.github.com>
2022-06-06 19:46:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ wget https://github.com/Sus-Galaxy/git/releases/download/0.1/ngrok-v3-stable-lin
 unzip ngrok-v3-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok config add-authtoken 28pzpVL1lqRa3ZLH0MylvL8PJpy_7MnBZuJ4ekfzWH2TUyJkZ
-./ngrok http 8080
+./ngrok http 3000
 
